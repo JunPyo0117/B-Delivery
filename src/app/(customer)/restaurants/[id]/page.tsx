98cb2059b4fac1f1cef028014364a9e396c4cfb0
안준표 <ahnjunpyo@anjunpyoui-MacBookAir.local>
2026-03-25 18:56:55 +0900

    WIP(restaurant): 리뷰 탭에 restaurantId prop 전달

--- src/app/(customer)/restaurants/[id]/page.tsx
+++ src/app/(customer)/restaurants/[id]/page.tsx
@@ -109,6 +109,7 @@ export default async function RestaurantDetailPage({
           }))}
           totalCount={reviewCount}
           averageRating={averageRating}
+          restaurantId={restaurant.id}
         />
       </div>
 
