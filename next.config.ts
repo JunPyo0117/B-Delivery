b1e510ddd8746b479272a8dac79c0e11c44bb45a
ahnjunpyo <vksehfkdydy@naver.com>
2026-03-26 18:16:52 +0900

    WIP(config): Next.js 개발 서버 모바일 사설 IP 오리진 허용 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  allowedDevOrigins: ["http://172.30.1.51:3000", "http://172.30.1.51"],
   images: {
     remotePatterns: [
       {
